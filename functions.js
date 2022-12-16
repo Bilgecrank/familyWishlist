1bbbd8f86d24881a6ab396e28a72ada722e30423
Bilgecrank <8432868+Bilgecrank@users.noreply.github.com>
2022-12-16 13:16:18 -0800

    Achieved basic functionality

--- functions.js
+++ functions.js
@@ -1,36 +1,64 @@
-/*
+/**
  * Functions for use on the wishlist site.
+ * @author Bilgecrank
  */
 
-const scriptUrl = 'https://script.google.com/macros/s/AKfycbzl-bEITgeN-HigNswlNkleRi21SGV9EPkwm4tI7ZA0Jv7bw0yrjCDWfKkI0GeW7I1a/exec';
+const scriptUrl = 'https://script.google.com/macros/s/AKfycby5fzI9r6I3DDKWvKyYJ-EOmoyu0WrOyuu15RpucxHYWBZ4gLKviWYSUfwP5xCZ5Hq3/exec';
 
 grabData();
 
 async function grabData() {
+    /**
+     * Send request receive data from google sheet and then assemble the tables.
+     */
     let response = await fetch(scriptUrl + '?sheetType=family', {
-        method: 'GET'
+        method: 'GET',
+        credentials: 'omit'
     });
     var result = await response.json();
-    console.log(result);
     buildTables(result);
 };
 
+submissionForm.onsubmit = 
+    async function sendData(e) {
+        /**
+         * Simulates form submission without causing a redirect.
+         */
+        e.preventDefault();
+        let response = await fetch(scriptUrl, {
+            method: 'POST',
+            credentials: 'omit',
+            body: new FormData(submissionForm)
+        });
+        alert(await response.json());
+        document.getElementById('submissionForm').reset();
+    };
+
 function buildTables(tableData) {
-    let headers = Object.keys(tableData[0]);
-    let rawHTML = '<table border="1">';
-    rawHTML += '<tr>'; //Build headers.
-    for (i = 2; i < headers.length; i++) {
-        rawHTML += '<th>' + headers[i] + '</th>';
+    /**
+     * Build tables with item objects retrieved from @function grabData()
+     *
+     * @param {obj} tableData
+     */
+
+    for (let table in tableData){
+        let inputData = tableData[table];
+        let headers = Object.keys(inputData[0]);
+        let rawHTML = '<table border="1">';
+        rawHTML += '<tr>'; //Build headers.
+        for (i = 2; i < headers.length; i++) {
+            rawHTML += '<th>' + headers[i] + '</th>';
+        }
+        rawHTML += '</tr>';//End headers, start data.
+        for (let x in inputData){
+            rawHTML += '<tr>';
+            rawHTML += '<td>' + inputData[x].item + '</td>';
+            rawHTML += '<td>' + inputData[x].description + '</td>';
+            rawHTML += '</tr>';
+        }
+        rawHTML += '</table>';
+        document.getElementById(table).innerHTML = rawHTML;
     }
-    rawHTML += '</tr>';//End headers, start data.
-    for (let x in tableData){
-        rawHTML += '<tr>';
-        rawHTML += '<td>' + tableData[x].item + '</td>';
-        rawHTML += '<td>' + tableData[x].description + '</td>';
-        rawHTML += '</tr>';
-    }
-    rawHTML += '</table>';
-    document.getElementById('buildATable').innerHTML = rawHTML;
 }
 
 
